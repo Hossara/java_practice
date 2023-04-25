6dbe7bb0adfb5ede0c349a3600c135f2394d6cff
Hossein Araghi <hoseinaraghi84@gmail.com>
2023-04-25 21:07:43 +0430

    DataTypes doc

---
+++ src/DataTypes.java
@@ -0,0 +1,19 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        /* Data Types
+        * -----------
+        * Data types specify the different sizes and values that can be stored in the variable.
+        * There are two types of data types in Java:
+        *   - Primitive data types
+        *   - Non-primitive data types
+        *
+        * - Primitive data types -
+        *   The primitive data types include boolean, char, byte, short, int, long, float and double.
+        *
+        * - Non-primitive data types -
+        *   The non-primitive data types include Classes, Interfaces, and Arrays.
+        */
+
+
+    }
+}
